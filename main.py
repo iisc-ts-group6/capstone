3931ab76cc73a84800f2b8fbfe554f3cd39ed4d3
vkrntgit <vikrantpayal@protonmail.com>
2024-06-07 13:00:31 +0530

    Added SciQ dataset and a little code in main.py to read the train file

--- main.py
+++ main.py
@@ -1,7 +1,10 @@
 import pandas as pd
 
 # Loading SciEntsBank to a dataframe
+df_sci_ents = pd.read_parquet('data/data-SciEntsBank/train-00001.parquet')
+print("SciEntsBank dataset shape : ", df_sci_ents.shape)
 
-df = pd.read_parquet('data/data-SciEntsBank/train-00001.parquet')
-print(df.shape)
+# Loading SciQ dataset to a dataframe
+df_sci_q = pd.read_parquet('data/data-SciQ/train-00000-of-00001.parquet')
+print("SciQ dataset shape : ", df_sci_q.shape)
 
