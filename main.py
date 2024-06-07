bd5941fd986c57f2353e42422c8548656b61f682
vkrntgit <vikrantpayal@protonmail.com>
2024-06-07 14:10:02 +0530

    Replaced dataset loading steps with a function

--- main.py
+++ main.py
@@ -1,10 +1,10 @@
 import pandas as pd
 
-# Loading SciEntsBank to a dataframe
-df_sci_ents = pd.read_parquet('data/data-SciEntsBank/train-00001.parquet')
-print("SciEntsBank dataset shape : ", df_sci_ents.shape)
+def load_dataset(loc: str) -> pd.DataFrame:
+    return pd.read_parquet(loc)
 
-# Loading SciQ dataset to a dataframe
-df_sci_q = pd.read_parquet('data/data-SciQ/train-00000-of-00001.parquet')
-print("SciQ dataset shape : ", df_sci_q.shape)
+loc = 'data/data-SciEntsBank/train-00001.parquet'
+df = load_dataset(loc)
+print('Dataset shape : ', df.shape)
+
 
