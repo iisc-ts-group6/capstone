c91219fbb00c453fba2de3ed12f99570bed232bb
Vikrant Payal <vikrantpayal@gmail.com>
2024-06-07 17:02:20 +0000

    added comments on prerequisite and introduced pdf in load_dataset

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
+# Prerequisite 
+# pip install pyarrow
+
 import pandas as pd
 
-def load_dataset(loc: str) -> pd.DataFrame:
-    return pd.read_parquet(loc)
+def load_dataset(loc: str, doc_type: str):
+    if doc_type == 'par': # parquet
+        return pd.read_parquet(loc)
+    elif doc_type == 'pdf':
+        return True # code pending. return a doc object
 
 loc = 'data/data-SciEntsBank/train-00001.parquet'
-df = load_dataset(loc)
+doc_type = 'par'
+df = load_dataset(loc, doc_type)
 print('Dataset shape : ', df.shape)
+
+
 
 
