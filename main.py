d5e94f034ae2b4be929345239fbc3b713f17579b
Vikrant Payal <vikrantpayal@gmail.com>
2024-06-08 00:20:47 +0000

    For reading pdf dataset, replaced pypdf with langchain_community.document_loaders

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 # Prerequisite 
 # pip install pandas
 # pip install langchain
-# pip install pypdf
+# pip install langchain_community
 # pip install pyarrow
 
 import pandas as pd
 import langchain
-from pypdf import PdfReader
+from langchain_community.document_loaders import PyPDFLoader
 
 
 
@@ -14,14 +14,15 @@ def load_dataset(doc_loc: str, doc_type: str):
     if doc_type == 'par': # parquet
         return pd.read_parquet(doc_loc)
     elif doc_type == 'pdf':
-        text = ""
-        try:
-            reader = PdfReader(doc_loc)
-            for page in reader.pages:
-                text += page.extract_text()
-        except Exception as exc:
-            print("ERROR: pdf extraction failed") #text = fallback_text_extraction("example.pdf")
-        return text 
+        # Load PDF
+        loaders = [
+        # Duplicate documents on purpose
+        PyPDFLoader(doc_loc),
+        ]
+        docs = []
+        for loader in loaders:
+            docs.extend(loader.load())
+        return docs
     return "ERROR: Dataset load failed. Check file type. Only parquet and pdf supported."
 
 
@@ -35,6 +36,3 @@ doc_loc = 'data/10/jesc1an.pdf'
 doc_type = 'pdf'
 pdf_text = load_dataset(doc_loc, doc_type)
 print('PDF dataset :' , pdf_text)
-
-
-
