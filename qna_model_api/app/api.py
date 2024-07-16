3c805217e5ef646c0306c9240da2306d691283ae
sampathmurala <sam507cse@gmail.com>
2024-07-16 11:58:00 +0530

    updated api details

--- qna_model_api/app/api.py
+++ qna_model_api/app/api.py
@@ -42,23 +42,23 @@ async def predict():
    # Todo: Comment below line once vectorstore db is persistent with splits
     return run_pipeline()
     
-@api_router.post("/predict", response_model= Any)
-async def predict(data: schemas.UserQuery):
-    rm = rag_model()
-    question = data.question
-    result = rm.get_llm_answer(query=question)
-    print(result)
-    student_answer = data.user_input
-    llm_answer = result['answer']
+# @api_router.post("/predict", response_model= Any)
+# async def predict(data: schemas.UserQuery):
+#     rm = rag_model()
+#     question = data.question
+#     result = rm.get_llm_answer(query=question)
+#     print(result)
+#     student_answer = data.user_input
+#     llm_answer = result['answer']
     
-    print(student_answer)
-    print(llm_answer)
-    pred_obj = sts_predict_score()
-    score = pred_obj.predict(llm_answer, student_answer)
-    print(score)
-    response = { 'question': question , 'student_answer': student_answer , 
-                 'llm_answer':llm_answer , 'score': str(score)}
-    return response
+#     print(student_answer)
+#     print(llm_answer)
+#     pred_obj = sts_predict_score()
+#     score = pred_obj.predict(llm_answer, student_answer)
+#     print(score)
+#     response = { 'question': question , 'student_answer': student_answer , 
+#                  'llm_answer':llm_answer , 'score': str(score)}
+#     return response
 
 def row_to_dict(row: pd.Series) -> schemas.ResponseObj:
     try:
