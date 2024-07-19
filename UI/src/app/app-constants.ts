850c020876b1f90b745be69c73cf58fb85ded72e
guru2611 <57456476+guru2611@users.noreply.github.com>
2024-07-19 13:48:44 +0530

    Update app-constants.ts

--- UI/src/app/app-constants.ts
+++ UI/src/app/app-constants.ts
@@ -1,7 +1,8 @@
 export class AppConstants {
     // if env local 
-    public static readonly baseURL: string = 'http://localhost:8001';
+    //public static readonly baseURL: string = 'http://localhost:8001';
     // if env other
   //  public static readonly baseURL: string = 'http://13.127.168.216:8001';  
   //public static readonly baseURL: string = 'http://13.233.10.59:8001';  
+  public static readonly baseURL: string = 'http://172.31.34.240:8001';  
 }
