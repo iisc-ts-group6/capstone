f16617c730a84e4ad9a4b561b847fef85c9d9dcd
guru2611 <57456476+guru2611@users.noreply.github.com>
2024-07-19 14:16:12 +0530

    Update app-constants.ts

--- UI/src/app/app-constants.ts
+++ UI/src/app/app-constants.ts
@@ -4,5 +4,5 @@ export class AppConstants {
     // if env other
   //  public static readonly baseURL: string = 'http://13.127.168.216:8001';  
   //public static readonly baseURL: string = 'http://13.233.10.59:8001';  
-  public static readonly baseURL: string = 'http://172.31.34.240:8001';  
+  public static readonly baseURL: string = 'http://13.201.94.44:8001';  
 }
